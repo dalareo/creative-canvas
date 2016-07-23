05c5836a489e6d793e407d4c36d4b2e157226867
David A. Lareo <dalareo@gmail.com>
2016-07-23 21:15:55 +0200

    remove check

--- imports/ui/box.js
+++ imports/ui/box.js
@@ -5,12 +5,6 @@ import { Boxes } from '../api/boxes.js';
 import './box.html';
 
 Template.box.events({
-  'click .toggle-checked'() {
-    // Set the checked property to the opposite of its current value
-    Boxes.update(this._id, {
-      $set: { checked: ! this.checked },
-    });
-  },
   'click .close'() {
     Boxes.remove(this._id);
   },
