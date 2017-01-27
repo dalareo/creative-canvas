05ea0fb2f841ea131718fc49c8f5b350b58494e5
David A. Lareo <dalareo@gmail.com>
2017-01-27 19:04:45 +0100

    Cards movement is now reactive!!

--- imports/ui/box.js
+++ imports/ui/box.js
@@ -5,22 +5,23 @@ import { Boxes } from '../api/boxes.js';
 
 import './box.html';
 
-Template.box.onRendered(function (event) {
+//Unnecesary as long as {{dataX}} and {{dataY}} are now reactive
+  //Template.box.onRendered(function (event) {
   // refresh boxes positions
-  var target = this.firstNode,
+  //var target = this.firstNode,
       // keep the dragged position in the data-x/data-y attributes
-      x = target.getAttribute('data-x');
-      y = target.getAttribute('data-y');
+      //x = target.getAttribute('data-x');
+      //y = target.getAttribute('data-y');
 
   // translate the element
-  target.style.webkitTransform =
-  target.style.transform =
-    'translate(' + x + 'px, ' + y + 'px)';
+  //target.style.webkitTransform =
+  //target.style.transform =
+    //'translate(' + x + 'px, ' + y + 'px)';
 
   // update the posiion attributes
-  target.setAttribute('data-x', x);
-  target.setAttribute('data-y', y);
-});
+  //target.setAttribute('data-x', x);
+  //target.setAttribute('data-y', y);
+//});
 
 Template.box.events({
 
