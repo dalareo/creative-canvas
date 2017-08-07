07698c012fcc0621a3242e63e5c383e99a39202a
David A. Lareo <dalareo@gmail.com>
2017-08-07 18:26:55 +0200

    added LDAP support

--- server/main.js
+++ server/main.js
@@ -13,7 +13,27 @@ Router.route('/canvas');
 // when you navigate to "/two" automatically render the template named "Two".
 Router.route('/help');
 
+// LDAP settings
+LDAP.bindValue = function (usernameOrEmail, isEmailAddress, FQDN) {
+    return ((isEmailAddress) ? usernameOrEmail : 'CN=' + usernameOrEmail + ',CN=users,OU=groups,DC=educaas,DC=io');
+}
+
+LDAP.generateSettings = function (request) {
+    return {
+        "serverDn": "CN=" + request.username + ",CN=users,OU=groups,DC=educaas,DC=io",
+        "serverUrl": "ldap://139.162.204.94:389",
+        "whiteListedFields": [ "uid", "cn", "givenName", "mail"],
+        "autopublishFields": [ "givenName"],
+        "searchField": [ "cn" ],
+        "searchValueType": [ "username" ]
+        };
+}
+
+LDAP.filter = function (isEmailAddress, usernameOrEmail, FQDN) { return '(&(' + ((isEmailAddress) ? 'mail' : 'cn') + '=' + usernameOrEmail + ')(objectClass=inetOrgPerson))'; }
+
+//LDAP.tryDBFirst = true;
+
+
 Meteor.startup(() => {
   // code to run on server at startup
-
 });
